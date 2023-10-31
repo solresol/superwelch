64d42d22d71a871bd58e7d2b769e1eb2a6e31701
Greg Baker <gregb@ifost.org.au>
2023-10-31 20:59:29 +1100

    Almost the same as BMAP3, but using random forests instead.

---
+++ demo/cli/bmap1_infer.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python3
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--model-database", required=True, help="SQLite database where train/bmap1.py ran")
+parser.add_argument("--session-database", required=True, help="Database where we will store the values that users enter (probably via the steam deck)")
+import sqlite3
+import sys
+from io import BytesIO
+import joblib
+import re
+import scipy
+import pandas
+
+args = parser.parse_args()
+
+model_conn = sqlite3.connect(args.model_database)
+cursor = model_conn.cursor()
+cursor.execute("SELECT model FROM bmap1_storage")
+row = cursor.fetchone()
+if row is None:
+    sys.exit(f"{args.model_database} does not contain a trained BMAP1 model")
+
+model_buffer = BytesIO(row[0])
+model = joblib.load(model_buffer)
+# Now `model` is the deserialized scikit-learn pipeline
+cursor.execute("select control_group_size, experiment_group_size from metadata")
+row = cursor.fetchone()
+if row is None:
+    sys.exit(f"Metadata in {args.sqlite_database} has not been initialized correctly")
+control_group_size, experiment_group_size = row
+
+control_columns = [f"control{i+1}" for i in range(control_group_size)]
+experiment_columns = [f"experimental{i+1}" for i in range(experiment_group_size)]
+select_args = ", ".join(control_columns + experiment_columns)
+create_defs = [f"{x} float" for x in (control_columns + experiment_columns)]
+columns = "\n, ".join(create_defs)
+
+word_completions = ['new', 'test', 'failed', 'succeeded'] + [f"{x}=" for x in (control_columns + experiment_columns) ]
+
+model_conn.close()
+
+
+conn = sqlite3.connect(args.session_database)
+cursor = conn.cursor()
+cursor.execute("pragma foreign_keys = on;")
+cursor.execute(f"""create table if not exists real_experiments (
+  real_experiment_id integer primary key,
+  experiment_started timestamp default current_timestamp,
+  {columns},
+  experiment_evaluated timestamp default current_timestamp,
+  welch_ttest_result bool,
+  welch_ttest_pvalue float,
+  bmap1_result bool,
+  bmap1_pvalue float,
+  bmap3_result bool,
+  bmap3_pvalue float,
+  ground_truth bool
+)""")
+
+cursor.execute(f"""create table if not exists current_experiment (
+  real_experiment_id integer references real_experiments
+)""")
+
+
+if sys.stdin.isatty():
+    from prompt_toolkit import PromptSession, print_formatted_text
+    from prompt_toolkit.formatted_text import FormattedText
+    from prompt_toolkit.completion import WordCompleter
+    session = PromptSession()
+    word_completer = WordCompleter(word_completions)
+else:
+    print_formatted_text = print
+    FormattedText = lambda x: "".join([t[1] for t in x])
+
+
+
+
+all_input_consumed = False
+
+while True:
+    cursor.execute("select real_experiment_id from current_experiment")
+    row = cursor.fetchone()
+    if row is None:
+        current_experiment_id = None
+    else:
+        current_experiment_id = row[0]
+
+    if all_input_consumed:
+        break
+    try:
+        if sys.stdin.isatty():
+            if current_experiment_id is None:
+                prompt = "BMAP1-cli (no experiment): "
+                c = WordCompleter(['new'])
+            else:
+                prompt = f"BMAP1-cli #{current_experiment_id}: "
+                c = word_completer
+            user_input = session.prompt(prompt, completer=c)
+        else:
+            user_input = sys.stdin.read()
+            all_input_consumed = True
+    except EOFError:
+        break
+
+    if user_input.strip() == '':
+        continue
+    # Crappy command interpreter.
+    if user_input.lower().strip().startswith("new"):
+        print_formatted_text(FormattedText([('fg:blue', "Creating a new experiment...")]), end='')
+        cursor.execute("insert into real_experiments (experiment_started) values (current_timestamp) returning real_experiment_id")
+        row = cursor.fetchone()
+        current_experiment_id = row[0]
+        cursor.execute("delete from current_experiment;")
+        cursor.execute("insert into current_experiment (real_experiment_id) values (?)", [current_experiment_id])
+        conn.commit()
+        print_formatted_text(FormattedText([('fg:blue', f" experiment ID = {current_experiment_id}...")]))
+        continue
+
+    if current_experiment_id is not None and '=' in user_input:
+        l, r = user_input.split('=', 1)
+        l = l.strip().lower()
+        r = r.strip().lower()
+        if l.startswith('control') or l.startswith('experimental'):
+            try:
+                if l.startswith('control'):
+                    which_var = l[len('control'):]
+                    which_var = int(which_var)
+                    if which_var > control_group_size + 1:
+                        print_formatted_text(FormattedText([('fg:red', f"There are only {control_group_size} controls")]))
+                        continue
+                if l.startswith('experimental'):
+                    which_var = l[len('experimental'):]
+                    which_var = int(which_var)
+                    if which_var > experiment_group_size + 1:
+                        print_formatted_text(FormattedText([('fg:red', f"There are only {experiment_group_size} experimental values")]))
+                        continue
+            except ValueError:
+                print_formatted_text(FormattedText([('fg:red', f"Column {l} was not understood")]))
+                continue
+            try:
+                set_value = float(r)
+            except ValueError:
+                print_formatted_text(FormattedText([('fg:red', f"Value {r} was not understood")]))
+                continue
+            cursor.execute(f"update real_experiments set {l} = ? where real_experiment_id = ?",
+                           [set_value, current_experiment_id])
+            conn.commit()
+            print_formatted_text(FormattedText([('fg:blue', f"{l} has been set to be {set_value}.")]))
+            continue
+
+    if current_experiment_id is not None and user_input.lower().strip().startswith("test"):
+        cursor.execute(f"select {select_args} from real_experiments where real_experiment_id = ?",
+                      [current_experiment_id])
+        row = cursor.fetchone()
+        if row is None:
+            sys.exit("Database integrity error")
+
+        control_values = []
+        experiment_values = []
+        for field, value in zip(control_columns + experiment_columns, row):
+            if field.startswith('control'):
+                control_values.append(value)
+            elif field.startswith("experimental"):
+                experiment_values.append(value)
+        welch_outcome = scipy.stats.ttest_ind(control_values, experiment_values, equal_var=False)
+        welch_pretty_result = "experiment succeeded" if welch_outcome.pvalue < 0.05 else "experiment failed"
+        welch_result = 1 if welch_outcome.pvalue < 0.05 else 0
+        print_formatted_text(FormattedText([('fg:green', f"Welch t-test reported: {welch_pretty_result} with p-value {welch_outcome.pvalue}")]))
+
+        bmap_df = pandas.DataFrame()
+        for (i,c) in enumerate(sorted(control_values)):
+            bmap_df[f"control{i+1}"] = [c]
+        for (i,x) in enumerate(sorted(experiment_values)):
+            bmap_df[f"experimental{i+1}"] = [x]
+
+        prediction = int(model.predict(bmap_df)[0])
+        probs = model.predict_proba(bmap_df)[0]
+        model_pretty_result = "experiment succeeded" if prediction == 1 else "experiment failed"
+        print_formatted_text(FormattedText([('fg:green', f"BMAP1 test reported: {model_pretty_result} with probability of it being successful = {probs[1]}")]))
+        cursor.execute("update real_experiments set welch_ttest_result = ?, welch_ttest_pvalue = ?, bmap1_result = ?, bmap1_pvalue = ? where real_experiment_id = ?",
+                       [welch_result, welch_outcome.pvalue, prediction, probs[1], current_experiment_id])
+        conn.commit()
+        continue
+
+    if current_experiment_id is not None and user_input.lower().startswith("fail"):
+        cursor.execute("update real_experiments set ground_truth = false where real_experiment_id = ?",
+                       [current_experiment_id])
+        conn.commit()
+        print_formatted_text(FormattedText([('fg:blue','Experiment marked as failed')]))
+        continue
+
+    if current_experiment_id is not None and user_input.lower().startswith("succ"):
+        cursor.execute("update real_experiments set ground_truth = true where real_experiment_id = ?",
+                       [current_experiment_id])
+        conn.commit()
+        print_formatted_text(FormattedText([('fg:blue','Experiment marked as succeeded')]))
+        continue
+
+
+
+    print_formatted_text(FormattedText([('fg:red', f"Command {user_input} was not understood")]))
+    #print_formatted_text(FormattedText([('bold',user_input)]))
+    # print_formatted_text(FormattedText([('fg:black',"Processing end reason: "), ('fg:yellow', finish_reason)]))
+    # print_formatted_text(FormattedText([('fg:yellow','Thinking...')]), end='\r')
+
+    # print_formatted_text(FormattedText([('fg:red', error_message)]))
+    # print_formatted_text(FormattedText([('fg:green', answer)]))
---
+++ demo/cli/bmap1_train.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--sqlite-database", required=True,
+                    help="Filename to read the experiments from")
+parser.add_argument("--force-retrain", action="store_true",
+                    help="Force retraining, even if it is unnecessary")
+args = parser.parse_args()
+
+import sys
+import sqlite3
+import pandas
+import sklearn.ensemble
+import sklearn.model_selection
+import joblib
+import numpy
+from io import BytesIO
+import differencer
+import typescorers
+
+conn = sqlite3.connect(args.sqlite_database)
+cursor = conn.cursor()
+cursor.execute("pragma foreign_keys = on;")
+cursor.execute("""create table if not exists bmap1_training (
+  type_1_error_ratio float,
+  type_2_error_ratio float,
+  roc_auc float,
+  accuracy float,
+  last_trained timestamp);
+""")
+cursor.execute("select control_group_size, experiment_group_size, most_recent_write from metadata")
+row = cursor.fetchone()
+if row is None:
+    sys.exit(f"Metadata in {args.sqlite_database} has not been initialized correctly")
+control_group_size, experiment_group_size, most_recent_write = row
+
+if not(args.force_retrain):
+    cursor.execute("select last_trained from bmap1_training")
+    row = cursor.fetchone()
+    if row is not None:
+        last_trained = row[0]
+        if most_recent_write < last_trained:
+            sys.exit("Nothing to do")
+
+
+control_columns = [f"control{i+1}" for i in range(control_group_size)]
+experiment_columns = [f"experimental{i+1}" for i in range(experiment_group_size)]
+select_args = ", ".join(control_columns + experiment_columns)
+
+raw_data_dataframe = pandas.read_sql(f"select {select_args}, was_successful from synthetic_experiments", conn)
+
+rfc = sklearn.ensemble.RandomForestClassifier(class_weight='balanced')
+
+
+scoring = sklearn.model_selection.cross_validate(
+    rfc,
+    raw_data_dataframe[control_columns + experiment_columns],
+    raw_data_dataframe.was_successful,
+    scoring={
+        'type_1_error_ratio': typescorers.type_1_error_scorer,
+        'type_2_error_ratio': typescorers.type_2_error_scorer,
+        'accuracy': sklearn.metrics.make_scorer(sklearn.metrics.accuracy_score),
+        'roc_auc': sklearn.metrics.make_scorer(sklearn.metrics.roc_auc_score)
+    }
+)
+
+mean_scores = {metric: numpy.mean(values) for metric, values in scoring.items()}
+data_to_insert = (
+    mean_scores['test_type_1_error_ratio'],
+    mean_scores['test_type_2_error_ratio'],
+    mean_scores['test_roc_auc'],
+    mean_scores['test_accuracy'],
+)
+
+cursor.execute("delete from bmap1_training;")
+cursor.execute("""INSERT OR REPLACE INTO bmap1_training
+    (type_1_error_ratio, type_2_error_ratio, roc_auc, accuracy, last_trained)
+    VALUES (?, ?, ?, ?, current_timestamp);
+""", data_to_insert)
+conn.commit()
+                                       
+rfc.fit(raw_data_dataframe[control_columns + experiment_columns],
+             raw_data_dataframe.was_successful)
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS bmap1_storage (
+    model BLOB,
+    saved_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);""")
+cursor.execute("delete from bmap1_storage")
+
+
+# Serialize the model
+model_buffer = BytesIO()
+joblib.dump(rfc, model_buffer)
+model_buffer.seek(0)  # Rewind the buffer to the beginning of the stream
+serialized_model = model_buffer.getvalue()
+
+# Insert the model into the database
+insert_query = """
+    INSERT model_storage (model)
+    VALUES (?, ?);
+"""
+cursor.execute("insert into bmap1_storage (model) values (?)", (serialized_model,))
+conn.commit()
